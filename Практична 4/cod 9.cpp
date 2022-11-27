2cf2da0363c4ebc36590c244e85b2612ef56c272
Traixxx <93382150+Traixxx@users.noreply.github.com>
2022-11-27 22:17:11 +0200

    Create cod 9.cpp

---
+++ Практична 4/cod 9.cpp
@@ -0,0 +1,26 @@
+// Example program
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+    
+   int ch1;
+    int ch2;
+    int ch3;
+    int ch4;
+
+    cin >> ch1;
+    cin >> ch2;
+    cin >> ch3;
+    cin >> ch4;
+
+    if (ch1, ch2, ch3, ch4 <= 255){
+        cout << ch1 << "." << ch2 << "." << ch3 << "." << ch4;
+      
+    }
+    else
+        cout << "wrong";
+
+return 0;
+}
